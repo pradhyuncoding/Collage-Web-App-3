23f9168c268275a516b258691c990a89f281262f
pradhyuncoding <84290612+pradhyuncoding@users.noreply.github.com>
2022-09-05 11:23:52 -0700

    Add files via upload

---
+++ main (1).js
@@ -0,0 +1,79 @@
+var SpeechRecognition = window.webkitSpeechRecognition;
+var Content;
+var recognition = new SpeechRecognition();
+
+function start()
+{
+    recognition.start();
+} 
+
+
+camera = document.getElementById("camera");
+Webcam.set({
+    width:500,
+    height:400,
+    image_format : 'jpeg',
+    jpeg_quality:90
+});
+
+
+
+function speak(){
+
+    
+    var synth = window.speechSynthesis;
+    Webcam.attach(camera);
+
+    speak_data = "Taking your next Selfie in 5 seconds";
+    var utterThis = new SpeechSynthesisUtterance(speak_data);
+    synth.speak(utterThis);
+
+
+}
+
+function take_snapshot()
+{
+    console.log(img_id);
+
+    Webcam.snap(function(data_uri){
+        if(img_id=="selfie1"){
+            document.getElementById("result1").innerHTML = '<img id="selfie1" src="'+data_uri+'"/>';
+        }
+        if(img_id=="selfie2"){
+            document.getElementById("result1").innerHTML = '<img id="selfie2" src="'+data_uri+'"/>';
+        }
+        if(img_id=="selfie3"){
+            document.getElementById("result1").innerHTML = '<imgid="selfie3" src="'+data_uri+'"/>';
+        }
+    });
+}
+
+setTimeout(function()
+{
+    imd_id = "selfie1"
+    take_snapshot();
+    speak_data = "Taking your next Selfie in 10 seconds";
+    var utterThis = new SpeechSynthesisUtterance(speak_data);
+    synth.speak(utterThis);
+
+}, 5000);
+
+setTimeout(function()
+{
+    imd_id = "selfie2"
+    take_snapshot();
+    speak_data = "Taking your next Selfie in 15 seconds";
+    var utterThis = new SpeechSynthesisUtterance(speak_data);
+    synth.speak(utterThis);
+
+}, 10000);
+
+setTimeout(function()
+{
+    imd_id = "selfie3"
+    take_snapshot();
+    speak_data = "Taking your next Selfie in 20 seconds";
+    var utterThis = new SpeechSynthesisUtterance(speak_data);
+    synth.speak(utterThis);
+
+}, 15000);
